d0e642022af5c9b3b7fa16b54d888afa80802bef
Scott Gibb <smgibb@yahoo.com>
2025-12-18 22:32:29 +0000

    feat: added some more tools

--- modules/gui-tools.nix
+++ modules/gui-tools.nix
@@ -2,11 +2,26 @@
 
 {
   home.packages = with pkgs; [
+    # IDES and Editors
     vscode
     obsidian
+
+    # Diagrams
+    drawio
+
+    # Messaging apps
+    telegram-desktop
+
+    ## CAD/ECAD Apps
     # kicad
+    # prusaslicer # Doesn't exist
+    # cura # Linux Only
+    # preform # Doesn't exist
+
   ] ++ lib.optionals pkgs.stdenv.isLinux [
     angryipscanner
+  ] ++ lib.optionals pkgs.stdenv.isDarwin [
+    # whatsapp-for-mac  # SSL certificate issue with download
   ];
 
 }
