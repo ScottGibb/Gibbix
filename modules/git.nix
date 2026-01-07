84be0e875d624c8bd7bb02838340e3cdce27fb28
Scott Gibb <44235263+ScottGibb@users.noreply.github.com>
2026-01-07 14:20:03 +0000

    feat: Enable delta program with Git integration

--- modules/git.nix
+++ modules/git.nix
@@ -9,4 +9,14 @@
       };
     };
   };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      "line-numbers" = true;
+      navigate = true;
+      "syntax-theme" = "Nord";
+    };
+  };
 }
