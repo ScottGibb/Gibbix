ed1709fdcab42f70b62fceeda9922ad1d837578d
Scott Gibb <smgibb@yahoo.com>
2026-02-20 15:44:00 +0000

    feat: add tauri xcode setup
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- hosts/personal-mac.nix
+++ hosts/personal-mac.nix
@@ -21,12 +21,12 @@
     fi
   '';
 
-programs.fish = {
-  enable = true;
-  shellInit = ''
-    # Set SDK for Xcode toolchain so Rust/C can link system libs
-    set -gx SDKROOT /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
-    set -gx PATH /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin $PATH
-  '';
-};
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      # Set SDK for Xcode toolchain so Rust/C can link system libs
+      set -gx SDKROOT /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+      set -gx PATH /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin $PATH
+    '';
+  };
 }
